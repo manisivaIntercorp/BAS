05adf6b6a881580031248da93569df5dbf3da4a4
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-01-20 12:19:13 +0530

    User Account Creation

---
+++ DataAccessLayer/Interface/IUserAccountDAL.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Interface
+{
+    public interface IUserAccountDAL
+    {
+        Task<List<UserAccountModel>> GetAllUserAccount();
+        Task<bool> InsertUpdateUserAccount(UserAccountModel UM);
+        Task<bool> DeleteUserAccount(int Id);
+        Task<UserAccountModel> GetUserAccountById(int Id);
+        Task<bool> UpdateUserAccountAsync(int id, UserAccountModel userAccount);
+    }
+}
