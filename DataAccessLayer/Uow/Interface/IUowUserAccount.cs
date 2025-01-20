759a2396f9d48c19f9e97b75f46d08e3d7cc68f8
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-01-20 12:13:28 +0530

    User Account Creation

---
+++ DataAccessLayer/Uow/Interface/IUowUserAccount.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessLayer.Interface;
+using DataAccessLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Uow.Interface
+{
+    public interface IUowUserAccount : IDisposable
+    {
+        IUserAccountDAL UserAccountDALRepo { get; }
+        void Commit();
+    }
+}
