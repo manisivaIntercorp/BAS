70972bdc0c8197a6b1c714482a2857c5cfd78f2a
AndrewsChrispin <andrews.john@intercorpsolutions.in>
2025-01-28 19:21:23 +0530

    Organisation check in DAL(Interface)

---
+++ DataAccessLayer/Interface/IOrganisationDAL.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Interface
+{
+    public interface IOrganisationDAL
+    {
+        Task<List<OrganisationModel>> GetAllOrganisation();
+
+        Task<OrganisationModel> GetOrganisationById(int Id);
+
+
+        Task<string> InsertOrganisation(OrganisationModel OrganisationModel);
+        Task<string> UpdateOrganisation(int id, OrganisationModel OrganisationModel);
+    }
+}
