7b9a8ab88b89095c690fdf518f957585b1ec1351
AndrewsChrispin <andrews.john@intercorpsolutions.in>
2025-01-28 19:26:16 +0530

    Organisation check In DAL(Uow-Interface)

---
+++ DataAccessLayer/Uow/Interface/IUowOrganisation.cs
@@ -0,0 +1,29 @@
+﻿using DataAccessLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Uow.Interface
+{
+    /// <summary>
+    /// Interface for Unit of Work specific to Organisation operations.
+    /// Manages transactions and repositories for Organisation data access.
+    /// </summary>
+    public interface IUowOrganisation : IDisposable
+    {
+        /// <summary>
+        /// Gets the Organisation Data Access Repository.
+        /// </summary>
+        IOrganisationDAL OrganisationDALRepo { get; }
+
+        /// <summary>
+        /// Commits the current transaction.
+        /// </summary>
+        void Commit();
+    }
+}
+
+
+
