0c811296316656fb8c950efbe187af0b110691e9
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-01-28 11:35:35 +0530

    User Policy Creation

---
+++ DataAccessLayer/Interface/IUserGroupDAL.cs
@@ -0,0 +1,19 @@
+﻿using DataAccessLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Interface
+{
+    public interface IUserGroupDAL
+    {
+        Task<List<UserGroupModel>> GetAllUserPolicy();
+        Task<bool> InsertUpdateUserPolicy(UserGroupModel UM);
+        Task<bool> DeleteUserPolicy(int Id);
+        Task<UserGroupModel> GetUserPolicyById(int Id);
+        Task<bool> UpdateUserPolicyAsync(int id, UserGroupModel userAccount);
+    }
+
+}
