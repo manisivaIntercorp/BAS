ac443281af4d95e7c90b1c2a9aa028ceb99ef871
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-01-28 11:31:11 +0530

    Language Creation

---
+++ DataAccessLayer/Interface/ILanguageDAL.cs
@@ -0,0 +1,21 @@
+﻿using DataAccessLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Interface
+{
+    public interface ILanguageDAL
+    {
+        Task<List<LanguageModel>> GetAllLanguage();
+        Task<List<LanguageNameEnum>> GetAllLanguageinDropdown();
+
+        Task<bool> InsertUpdateLanguage(LanguageModel LM);
+        
+        Task<bool> DeleteLanguage(int Id);
+        Task<LanguageModel> GetLanguageById(int Id);
+        Task<bool> UpdateLanguageAsync(int id, LanguageModel objModel);
+    }
+}
