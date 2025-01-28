a50c7acad524a3f8bdcb5a6e61327550f788d507
AndrewsChrispin <andrews.john@intercorpsolutions.in>
2025-01-28 20:14:38 +0530

    connection string program class

--- Program.cs
+++ Program.cs
@@ -6,38 +6,26 @@ using NLog.Web;
 
 
 var builder = WebApplication.CreateBuilder(args);
-//var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
 
-//var logger =  NLog.LogManager.Setup().LoadConfigurationFromAppSettings();
+// Bind the connection string from configuration
+string connectionString = builder.Configuration.GetConnectionString("connection");
 
-//logger
-// Configure NLog for ASP.NET Core
-builder.Logging.ClearProviders(); // Clears default logging providers.
-builder.Host.UseNLog(); // Use NLog as the logging provider.
-// Add services to the container.
+// Register UowOrganisation with DI
+builder.Services.AddScoped<IUowOrganisation>(sp => new UowOrganisation(connectionString));
 
 builder.Services.AddControllers();
-//builder.Services.AddScoped( IUowNationality, UowNationality);
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-    
-    var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+var app = builder.Build();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.UseCors(option => {
-    option.AllowAnyOrigin();
-    option.AllowAnyMethod();
-    option.AllowAnyHeader();
-});
+
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
+
 
