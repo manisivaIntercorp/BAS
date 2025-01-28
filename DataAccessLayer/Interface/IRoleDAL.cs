9e156acc6078b8f21c1966bb19a9bd699b6c52fd
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-01-28 11:32:35 +0530

    Role Creation

---
+++ DataAccessLayer/Interface/IRoleDAL.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Interface
+{
+    public interface IRoleDAL
+    {
+        Task<List<RoleModel>> GetAllRole();
+        Task<(List<RoleModel> roleModels, int RetVal,string Msg)> InsertUpdateRole(RoleModel LM);
+        Task<bool> DeleteRole(int Id);
+        Task<(RoleModel rolemodel,List<Modules> ModuleDatatable)> getModulesBasedonRole(long Roleid, string IsPayrollAccessible,long UserID );
+        Task<(List<RoleModel> roleModels, int RetVal, string Msg)> UpdateRoleAsync(int id, RoleModel roleModel);
+    }
+}
