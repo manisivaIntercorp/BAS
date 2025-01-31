fae90a6fc0d587fa6f70f3c135bf5f65867601fb
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-01-31 15:33:09 +0530

    Added Services and Session

--- Program.cs
+++ Program.cs
@@ -1,17 +1,34 @@
-using DataAccessLayer;
 using DataAccessLayer.Uow.Implementation;
 using DataAccessLayer.Uow.Interface;
-using NLog.Extensions.Logging;
-using NLog.Web;
+using WebApi.Services;
 
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add session services
+builder.Services.AddDistributedMemoryCache(); // Required for session
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // Set session timeout
+    options.Cookie.HttpOnly = true; // Prevents JavaScript access
+    options.Cookie.IsEssential = true; // Ensures session works without tracking consent
+});
+builder.Services.AddControllersWithViews();
+builder.Services.AddHttpContextAccessor(); // Required for accessing session in services
 
 // Bind the connection string from configuration
 string connectionString = builder.Configuration.GetConnectionString("connection");
 
 // Register UowOrganisation with DI
 builder.Services.AddScoped<IUowOrganisation>(sp => new UowOrganisation(connectionString));
+// Register UowEmail with DI
+builder.Services.AddScoped<IUowEmailTemplate>(sp => new UowEmailTemplate(connectionString));
+
+//Added Email Services with DI
+builder.Services.AddScoped<EmailServices>();
+
+//Added Session Services with DI
+builder.Services.AddScoped<SessionService>();
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
@@ -23,9 +40,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+//Session
+app.UseSession();
+app.UseRouting();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
-
-
