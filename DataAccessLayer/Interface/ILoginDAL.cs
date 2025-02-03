e95fc1fc32ca900c6a0de890d888eabc861bf08d
AndrewsChrispin <andrews.john@intercorpsolutions.in>
2025-02-03 16:01:38 +0530

    Login

---
+++ DataAccessLayer/Interface/ILoginDAL.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Interface
+{
+    public interface ILoginDAL
+    {
+        Task<List<ResultModel>> UserLogin(LoginModel loginModel);
+        Task<List<LoginModel>> GetUserID(LoginModel loginModel);
+    }
+}
