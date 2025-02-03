4cc6a5597d18bb6534d478e3409797301289fb7e
AndrewsChrispin <andrews.john@intercorpsolutions.in>
2025-02-03 16:45:18 +0530

    login JWT Authentication

---
+++ Services/JwtService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace WebApi.Services
+{
+    public class JwtService
+    {
+
+        private readonly string _key;
+
+        public JwtService(string key)
+        {
+            if (key.Length < 32)
+            {
+                throw new ArgumentException("The secret key must be at least 32 characters long.");
+            }
+            _key = key;
+        }
+
+        public string GenerateToken(string username)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var keyBytes = Encoding.UTF8.GetBytes(_key);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, username) }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(keyBytes), SecurityAlgorithms.HmacSha256
+                )
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
