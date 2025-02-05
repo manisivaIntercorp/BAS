4bf5c4617390977c3a5599e2fdcffd094c24aa9d
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-02-05 22:19:59 +0530

    Change the Interface Name

---
+++ DataAccessLayer/Interface/IEmailTemplateDAL.cs
@@ -0,0 +1,11 @@
+﻿using DataAccessLayer.Services;
+
+namespace DataAccessLayer.Interface
+{
+    public interface IEmailTemplateDAL
+    {
+        List<GetEmailTemplate?> GetEmailTemplate(EmailTemplate? emailTemplate);
+        List<MailServer?> GetMailServerConfig(MailServer? mailServer);
+    }
+
+}
