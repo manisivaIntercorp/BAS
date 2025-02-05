598ecf9b80ad6bc43e192f70a675e71ed07ec1a0
AndrewsChrispin <andrews.john@intercorpsolutions.in>
2025-02-05 13:22:32 +0530

    Global variable

---
+++ Services/AppGlobalVariableService.cs
@@ -0,0 +1,34 @@
+﻿namespace WebApi.Services
+{
+    public class AppGlobalVariableService
+    {
+
+        public string UserID { get; set; }
+        public string UserName { get; set; }
+        public string UserDisplayName { get; set; }
+        public string UserRoleID { get; set; }
+        public string UserRoleName { get; set; }
+        public string UserRoleType { get; set; }
+        public string DisplayPDPA { get; set; }
+        public string PayrollAccessible { get; set; }
+        public string UserAccessRightsCount { get; set; }
+        public string SetPassword { get; set; }
+        public string PasswordExpiry { get; set; }
+        public string ValidateOTP { get; set; }
+        public string IsProfileUser { get; set; }
+        public string IsSystemUser { get; set; }
+        public string UserImgPath { get; set; }
+
+        public string DBName { get; set; }
+
+        public string DBChange { get; set; }
+        public string GlobalUser { get; set; }
+        public string InstanceChange { get; set; }
+        public string InstanceName { get; set; }
+        public string DataBaseUserName { get; set; }
+        public string DataBasePassword { get; set; }
+        public string TimeOffset { get; set; }
+        public string TimeZoneID { get; set; }
+        public string IsAppuserTimeZone { get; set; }
+    }
+}
