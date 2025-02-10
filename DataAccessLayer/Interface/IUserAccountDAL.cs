a104fd4855ff619bbf5984602d8c78da671ffc8d
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-02-10 09:34:35 +0530

    User Account DAL Interface Changes done to insert into client db

--- DataAccessLayer/Interface/IUserAccountDAL.cs
+++ DataAccessLayer/Interface/IUserAccountDAL.cs
@@ -10,20 +10,21 @@ namespace DataAccessLayer.Interface
 
         Task<List<GetRoleName?>> getAllUserRoleinDropdown();
 
-        Task<(List<UserAccountModel?> insertroles,int? RetVal, string? Msg)> InsertUpdateUserAccount(UserAccountModel? UM);
+        Task<(List<UserAccountModel?> InsertedUsers, List<OrgDetails?> OrgDetails, int? RetVal, string? Msg)> InsertUpdateUserAccount(UserAccountModel? UM);
 
         Task<(List<DeleteRoleName?> deleteroles, int? RetVal, string? Msg)> DeleteRoleinUserAccount(DeleteRoleName RM);
-        
+
         Task<(List<RoleName?> Roles, int? RetVal, string? Msg)> AddRoleName(RoleName roleName);
-        Task<(List<ResetPassword?> PasswordReset, int? RetVal, string? Msg)> ResetPasswordinUserAccount(ResetPassword Passwordreset);
-        
+        Task<(List<ResetPassword?> PasswordReset, int? RetVal, string? Msg)> ResetPasswordinUserAccount(ResetPassword PasswordReset);
+
         Task<bool> DeleteUserAccount(int? Id);
-        
+
         Task<(GetUserAccount? userAccounts, List<GetUserAccountRole>? UserRoles, List<GetUserAccountOrg>? Org)> GetUserAccountById(int? Id);
-        
+
         Task<List<OrgDetails?>> GetOrgDetailsByUserId();
+
         Task<(List<UserAccountModel?> updateuseraccount, int? RetVal, string? Msg)> UpdateUserAccountAsync(int? id, UserAccountModel? userAccount);
-        
+
         Task<(List<UnlockUser?> unlockuser, int? RetVal, string? Msg)> UnlockUserAsync(UnlockUser? model);
     }
 }
