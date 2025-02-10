195d2e77ca79cd5cb4f7f3e9ae43839746d328d3
AndrewsChrispin <andrews.john@intercorpsolutions.in>
2025-02-10 18:52:01 +0530

    Login

--- Services/ConnectionStringMiddleware.cs
+++ Services/ConnectionStringMiddleware.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.Data.SqlClient;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+using DataAccessLayer.Services;
 
 namespace WebApi.Services
 {
@@ -7,48 +10,66 @@ namespace WebApi.Services
     {
         private readonly RequestDelegate _next;
         private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IServiceProvider _serviceProvider;
 
-        public ConnectionStringMiddleware(RequestDelegate next, IHttpContextAccessor httpContextAccessor)
+        // public ConnectionStringMiddleware(RequestDelegate next, IHttpContextAccessor httpContextAccessor, IServiceProvider serviceProvider)
+        public ConnectionStringMiddleware(RequestDelegate next, IHttpContextAccessor httpContextAccessor, IServiceProvider serviceProvider)
         {
             _next = next;
             _httpContextAccessor = httpContextAccessor;
+            _serviceProvider = serviceProvider;
+
         }
 
         public async Task InvokeAsync(HttpContext context)
         {
             string connectionString;
+
             var session = context.Session;
-            if (session.GetString("DBName") != null)
+            if (session == null)
             {
-                connectionString = BuildConnectionString(session.GetString("DBName"));
-            }
-            else if (session.GetString("InstanceName") != null &&
-                     session.GetString("InstanceChange") == "Y" &&
-                     session.GetString("DataBaseUserName") != null &&
-                     session.GetString("DataBasePassword") != null)
-            {
-                connectionString = BuildConnectionString(
-                    session.GetString("InstanceName"),
-                    session.GetString("DataBaseUserName"),
-                    session.GetString("DataBasePassword"),
-                    session.GetString("DBName"));
-            }
-            else
-            {
-                var config = context.RequestServices.GetService(typeof(IConfiguration)) as IConfiguration;
-                // Make sure "connection" exists in your appsettings.json
-                connectionString = config?.GetConnectionString("connection");
+                await _next(context);
+                return;
             }
 
-            // Set the connection string in HttpContext.Items for later retrieval.
-            context.Items["connection"] = connectionString;
+            // Create service scope and resolve EncryptedDecrypt
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var encryptedDecrypt = scope.ServiceProvider.GetRequiredService<EncryptedDecrypt>();
+
+                if (session.GetString("DBName") != null && !String.IsNullOrEmpty(session.GetString("DBName")) && session.GetString("InstanceChange") != "Y")
+                {
+                    connectionString = BuildConnectionString(session.GetString("DBName"));
+                }
+                else if (session.GetString("InstanceName") != null &&
+                         session.GetString("InstanceChange") == "Y" &&
+                         session.GetString("DataBaseUserName") != null &&
+                         session.GetString("DataBasePassword") != null)
+                {
+                 connectionString = "";
+                    connectionString = BuildConnectionString(
+                        encryptedDecrypt.Decrypt(session.GetString("InstanceName")),
+                        encryptedDecrypt.Decrypt(session.GetString("DataBaseUserName")),
+                        encryptedDecrypt.Decrypt(session.GetString("DataBasePassword")),
+                        session.GetString("DBName"));
+                }
+                else
+                {
+                    var config = context.RequestServices.GetService<IConfiguration>();
+                    connectionString = config?.GetConnectionString("connection");
+                }
+
+                // Set the connection string in HttpContext.Items
+                context.Items["connection"] = connectionString;
+            }
+
             await _next(context);
         }
 
         private string BuildConnectionString(string dbName)
         {
-            var config = _httpContextAccessor.HttpContext.RequestServices.GetService(typeof(IConfiguration)) as IConfiguration;
-            var connectionString = config.GetConnectionString("connection");
+            var config = _httpContextAccessor?.HttpContext?.RequestServices.GetService<IConfiguration>();
+            var connectionString = config?.GetConnectionString("connection");
             var builder = new SqlConnectionStringBuilder(connectionString)
             {
                 InitialCatalog = dbName
@@ -58,17 +79,18 @@ namespace WebApi.Services
 
         private string BuildConnectionString(string serverName, string userID, string password, string dbName)
         {
-            var config = _httpContextAccessor.HttpContext.RequestServices.GetService(typeof(IConfiguration)) as IConfiguration;
-            var connectionString = config.GetConnectionString("DefaultConnection");
+            var config = _httpContextAccessor?.HttpContext?.RequestServices.GetService<IConfiguration>();
+            var connectionString = config?.GetConnectionString("DefaultConnection");
             var builder = new SqlConnectionStringBuilder(connectionString)
             {
                 DataSource = serverName,
                 UserID = userID,
                 Password = password,
-                InitialCatalog = dbName
+                InitialCatalog = dbName,
+                TrustServerCertificate = true,
+                MultipleActiveResultSets=true
             };
             return builder.ToString();
         }
     }
-
 }
