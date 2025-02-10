e89922e2edd88b0c1ac17cea7e74f270f487a048
AndrewsChrispin <andrews.john@intercorpsolutions.in>
2025-02-10 19:09:35 +0530

    Login changes

--- DataAccessLayer/Implementation/LoginDAL.cs
+++ DataAccessLayer/Implementation/LoginDAL.cs
@@ -28,11 +28,40 @@ namespace DataAccessLayer.Implementation
                 throw new ArgumentNullException(nameof(Connection), "The database connection cannot be null.");
 
             using var multi = await Connection.QueryMultipleAsync(
-               "sp_Authentication",
-               dynamicParameters,
-               transaction: Transaction,
-               commandType: CommandType.StoredProcedure);
+                "sp_Authentication",
+                dynamicParameters,
+                transaction: Transaction,
+                commandType: CommandType.StoredProcedure);
 
+            var res = multi.Read<ResultModel>().ToList();
+
+            if (res.Any() && res[0].RetVal == 1) // Compare with integer
+            {
+                var details = multi.Read<LoginDetailModel>().ToList();
+                res[0].lstLoginDetails = details;
+            }
+
+            return res;
+        }
+
+
+        public async Task<List<ResultModel>> GetUserID(LoginModel objloginModel)
+        {
+            DynamicParameters dyParameter = new DynamicParameters();
+            dyParameter.Add("@Mode", "GET_USER_ID");
+            dyParameter.Add("@UserName", objloginModel.UserName);
+            dyParameter.Add("@Password", objloginModel.Password);
+            dyParameter.Add("@UserType", objloginModel.UserType);
+            dyParameter.Add("@IPAddress", objloginModel.IPAddress);
+            dyParameter.Add("@DeviceName", objloginModel.DeviceName);
+            dyParameter.Add("@BrowserName", objloginModel.BrowserName);
+            if (Connection == null)
+                throw new ArgumentNullException(nameof(Connection), "The database connection cannot be null.");
+
+            var multi = await Connection.QueryMultipleAsync("sp_Authentication",
+                dyParameter,
+                transaction: Transaction,
+                commandType: CommandType.StoredProcedure);
             var res = multi.Read<ResultModel>().ToList();
             var details = multi.Read<LoginDetailModel>().ToList();
 
@@ -43,29 +72,21 @@ namespace DataAccessLayer.Implementation
             return res;
         }
 
-
-        public async Task<List<LoginModel>> GetUserID(LoginModel objloginModel)
+        public async Task<List<OrganisationDBDetails>> GetOrganisationWithDBDetails(LoginModel objloginModel)
         {
-            DynamicParameters dyParameter = new DynamicParameters();
-            dyParameter.Add("@Mode", objloginModel.Mode);
-            dyParameter.Add("@UserName", objloginModel.UserName);
-            dyParameter.Add("@Password", objloginModel.Password);
-            dyParameter.Add("@UserType", objloginModel.UserType);
-            dyParameter.Add("@IPAddress", objloginModel.IPAddress);
-            dyParameter.Add("@DeviceName", objloginModel.DeviceName);
-            dyParameter.Add("@BrowserName", objloginModel.BrowserName);
+            DynamicParameters dynamicParameters = new DynamicParameters();
+            dynamicParameters.Add("@Mode", "GET_ORG");
+            dynamicParameters.Add("@UserName", objloginModel.UserName);
             if (Connection == null)
-                throw new ArgumentNullException(nameof(Connection), "The database connection cannot be null.");
+                throw new ArgumentException(nameof(Connection), "The database connection cannot be null.");
 
-            var mulit = await Connection.QueryMultipleAsync("sp_Authentication",
-                dyParameter,
+            var multi = await Connection.QueryMultipleAsync("sp_Authentication",
+                dynamicParameters,
                 transaction: Transaction,
                 commandType: CommandType.StoredProcedure);
-            var res = mulit.Read<LoginModel>().ToList();
-
+            var res = multi.Read<OrganisationDBDetails>().ToList();
             return res;
         }
-
 
     }
 }
