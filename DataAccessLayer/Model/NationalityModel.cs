e6b6f6f5bafc4d19492d87d7a05c29247740ab3a
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-02-10 19:06:20 +0530

    CRUD Operations done in single sp

--- DataAccessLayer/Model/NationalityModel.cs
+++ DataAccessLayer/Model/NationalityModel.cs
@@ -6,5 +6,17 @@
         public string? Nationality { get; set; }
         public string? NationalityCode { get; set; }
         public string? Active { get; set; }
+        public int? CreatedBy { get; set; }
+        
+        public string? CreatedName { get; set; }
+        
+        public string? ModifiedName { get; set; }
+       
+        public DateTime? CreatedDateTime { get; set; }
+        public int? ModifiedBy { get; set; }
+        public DateTime? ModifiedDateTime { get; set; }
+       
+        public DateTime? LastActiveDate { get; set; }
     }
+    
 }
