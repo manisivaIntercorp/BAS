047a71ae6b8514b06178c8016a59b5325ea27ad9
AndrewsChrispin <andrews.john@intercorpsolutions.in>
2025-02-11 21:21:54 +0530

    Login Issue fixed

--- DataAccessLayer/Interface/ILoginDAL.cs
+++ DataAccessLayer/Interface/ILoginDAL.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccessLayer.Model;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +10,13 @@ namespace DataAccessLayer.Interface
 {
     public interface ILoginDAL
     {
+        
+        Task<List<ResultModel>> ClientUserLogin(LoginModel loginModel);
         Task<List<ResultModel>> UserLogin(LoginModel loginModel);
         Task<List<ResultModel>> GetUserID(LoginModel loginModel);
         Task<List<OrganisationDBDetails>> GetOrganisationWithDBDetails(LoginModel loginModel);
+
+        Task<List<GetDropDownDataModel>> GetDDlLanguage(string Mode, string RefID1, string RefID2, string RefID3);
+        Task<List<GetDropDownDataModel>> GetDDlModule(string Mode, string RefID1, string RefID2, string RefID3);
     }
 }
