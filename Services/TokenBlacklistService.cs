ac37cd60e3fadddd5699b8a1dad72b09e50aaee5
AndrewsChrispin <andrews.john@intercorpsolutions.in>
2025-02-12 10:26:19 +0530

    Logout functionality

---
+++ Services/TokenBlacklistService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Concurrent;
+
+namespace WebApi.Services
+{
+    public class TokenBlacklistService
+    {
+        private readonly ConcurrentDictionary<string, DateTime> _revokedTokens = new();
+
+        public void RevokeToken(string token, DateTime expiration)
+        {
+            _revokedTokens[token] = expiration;
+        }
+
+        public bool IsTokenRevoked(string token)
+        {
+            return _revokedTokens.TryGetValue(token, out var expiration) && expiration > DateTime.UtcNow;
+        }
+    }
+}
