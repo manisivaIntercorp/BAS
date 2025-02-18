5189d84af8bac5ed1a7924cebbca3d095c68f59b
AndrewsChrispin <andrews.john@intercorpsolutions.in>
2025-02-18 21:48:22 +0530

    Audit log

---
+++ Services/Implementation/AuditLogService.cs
@@ -0,0 +1,41 @@
+﻿using DataAccessLayer.Implementation;
+using DataAccessLayer.Interface;
+using DataAccessLayer.Model;
+using DataAccessLayer.Uow.Interface;
+using WebApi.Services;
+using WebApi.Services.Interface;
+
+
+namespace WebApi.Services.Implementation
+{
+    public class AuditLogService : IAuditLogService
+    {
+        private readonly IUowAuditLog _uowAuditLog;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public AuditLogService(IUowAuditLog uowAuditLog, IHttpContextAccessor httpContextAccessor)
+        {
+            _uowAuditLog = uowAuditLog;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task LogAction(string UserGuid, string action, string? token = null)
+        {
+
+
+            var httpContext = _httpContextAccessor.HttpContext;
+            var auditLog = new AuditLog
+            {
+                UserGuid = UserGuid,
+                Action = action,
+                Token = token,
+                IPAddress = httpContext?.Connection?.RemoteIpAddress?.ToString(),
+                DeviceInfo = httpContext?.Request?.Headers["User-Agent"].ToString(),
+                CreatedDateTime = DateTime.UtcNow
+            };
+
+            await _uowAuditLog.AuditLogDALRepo.LogAudit(auditLog);
+        }
+    }
+}
+
