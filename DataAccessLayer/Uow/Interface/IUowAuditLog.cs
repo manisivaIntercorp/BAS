2a5fc22cf96f6b846447e1117b538a9fdd313f0b
AndrewsChrispin <andrews.john@intercorpsolutions.in>
2025-02-18 22:06:05 +0530

    Audit log

---
+++ DataAccessLayer/Uow/Interface/IUowAuditLog.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataAccessLayer.Uow.Interface
+{
+    public interface IUowAuditLog
+    {
+        IAuditLogDAL AuditLogDALRepo { get; }
+        void Commit();
+    }
+}
