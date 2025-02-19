9d5f6252e2034790354f6b1b210e9085dddae2a5
AndrewsChrispin <andrews.john@intercorpsolutions.in>
2025-02-19 10:37:42 +0530

    Audit log

---
+++ DataAccessLayer/Model/AuditLog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DataAccessLayer.Model
+{
+        public class AuditLog
+    {
+            public long Id { get; set; }
+            public long? UserId { get; set; }
+            public string? UserGuid { get; set; }
+            public string? Mode { get; set; }
+            public string? Token { get; set; }
+            public string? Action { get; set; }
+            //public string ActionType { get; set; } = string.Empty;
+            //public string? TableName { get; set; }
+            //public string? OldValues { get; set; }
+            //public string? NewValues { get; set; }
+            //public string? AffectedColumns { get; set; }
+            //public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+            public string? IPAddress { get; set; }
+            public string? DeviceInfo { get; set; }
+            public string? CreatedBy { get; set; }
+            public DateTime? CreatedDateTime { get; set; }
+            public string? ModifiedBy { get; set; }
+            public DateTime? ModifiedDateTime { get; set; }
+            public DateTime? UTCCreatedDateTime { get; set; }
+            public DateTime? UTCModifiedDateTime { get; set; }
+    }    
+}
