46375e1379941fffa865ea3510dacbeab67cf626
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-02-19 19:36:07 +0530

    Change the ID to GUID

--- DataAccessLayer/Interface/IUserAccountDAL.cs
+++ DataAccessLayer/Interface/IUserAccountDAL.cs
@@ -4,27 +4,28 @@ namespace DataAccessLayer.Interface
 {
     public interface IUserAccountDAL
     {
-        Task<List<UserAccountModel?>> GetAllUserAccount();
+        Task<List<GetUserAccountModel?>> GetAllUserAccount();
 
-        Task<List<UserPolicyName?>> getAllUserPolicyinDropdown();
+        Task<List<UserPolicyName?>> getAllUserPolicyInDropdown();
 
-        Task<List<GetRoleName?>> getAllUserRoleinDropdown();
+        Task<List<GetRoleName?>> getAllUserRoleInDropdown();
 
         Task<(List<UserAccountModel?> InsertedUsers, List<OrgDetails?> OrgDetails, int? RetVal, string? Msg)> InsertUpdateUserAccount(UserAccountModel? UM);
 
-        Task<(List<DeleteRoleName?> deleteroles, int? RetVal, string? Msg)> DeleteRoleinUserAccount(DeleteRoleName RM);
+        Task<(List<DeleteRoleName?> deleteroles, int? RetVal, string? Msg)> DeleteRoleInUserAccount(DeleteRoleName RM);
 
         Task<(List<RoleName?> Roles, int? RetVal, string? Msg)> AddRoleName(RoleName roleName);
-        Task<(List<ResetPassword?> PasswordReset, int? RetVal, string? Msg)> ResetPasswordinUserAccount(ResetPassword PasswordReset);
+        Task<(List<ResetPassword?> PasswordReset, int? RetVal, string? Msg)> ResetPasswordInUserAccount(ResetPassword PasswordReset);
 
-        Task<(bool deleteuseraccount, List<DeleteResult> deleteResults)> DeleteUserAccount(int UpdatedBy,DeleteUserAccount deleteUserAccount);
+        Task<(bool deleteuseraccount, List<DeleteResult> deleteResults)> DeleteUserAccount(long UpdatedBy,DeleteUserAccount deleteUserAccount);
 
-        Task<(GetUserAccount? userAccounts, List<GetUserAccountRole>? UserRoles, List<GetUserAccountOrg>? Org)> GetUserAccountById(int? Id);
-        //Task<(GetUserAccount? userAccounts, List<GetUserAccountRole>? UserRoles, List<GetUserAccountOrg>? Org)> GetUserAccountByName(string? UserName);
-
-        Task<List<OrgDetails?>> GetOrgDetailsByUserId();
+        Task<(GetUserAccount? userAccounts, List<GetUserAccountRole>? UserRoles, List<GetUserAccountOrg>? Org)> GetUserAccountByGUId(string? GUId);
+        Task<List<OrgDetails?>> GetOrgDetailsByUserGUId();
 
         Task<(List<UserAccountModel?> updateuseraccount, int? RetVal, string? Msg)> UpdateUserAccountAsync(UserAccountModel? userAccount);
+
+
+        Task<(List<UserAccountModel?> updateuseraccount, int? RetVal, string? Msg)> EditUpdateUserAccountAsync(UserAccountModel? userAccount);
 
         Task<(List<UnlockUser?> unlockuser, int? RetVal, string? Msg)> UnlockUserAsync(UnlockUser? model);
     }
