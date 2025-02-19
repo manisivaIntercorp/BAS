ea125f77a999f2f4af2dc9a0c4c99a0d57fd20fe
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-02-19 19:14:10 +0530

    Added GUID Service

---
+++ DataAccessLayer/Uow/Interface/IUowGUID.cs
@@ -0,0 +1,12 @@
+﻿using DataAccessLayer.Implementation;
+using DataAccessLayer.Model;
+
+namespace DataAccessLayer.Uow.Interface
+{
+     public interface IUowGUID : IDisposable
+    {
+        public GUIDDAL GUIDDALRepo { get; }
+        void Commit();
+
+    }
+}
