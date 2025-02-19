e84cb0ad16bd43c3b1e27022573535498c4d148d
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-02-19 19:16:44 +0530

    Added GUID Service

---
+++ DataAccessLayer/Interface/IGUIDDAL.cs
@@ -0,0 +1,15 @@
+﻿
+
+using DataAccessLayer.Implementation;
+using DataAccessLayer.Services;
+
+namespace DataAccessLayer.Interface
+{
+    public interface IGUIDDAL
+    {
+        Task<(bool GetGuid, int RetVal, string Msg)> GetGUID(string UserGuid);
+        Task<(bool GetGuid, int RetVal, string Msg)> GetGUIDBasedOnUserAccountRoleGuid(string UserGuid);
+        Task<(bool GetGuid, int RetVal, string Msg)> GetGUIDBasedOnRoleGuid(string UserGuid);
+
+    }
+}
