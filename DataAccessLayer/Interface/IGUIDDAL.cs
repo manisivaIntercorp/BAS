dd7febeb3b54791fdace3c5e0c7cb2ac3318aa0a
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-02-24 07:22:16 +0530

    Added the Org Name and User Policy

--- DataAccessLayer/Interface/IGUIDDAL.cs
+++ DataAccessLayer/Interface/IGUIDDAL.cs
@@ -7,9 +7,11 @@ namespace DataAccessLayer.Interface
 {
     public interface IGUIDDAL
     {
-        Task<(bool GetGuid, int RetVal, string Msg)> GetGUID(string UserGuid);
+        Task<(bool GetGuid, int RetVal, string Msg)> GetGUID(string? UserGuid);
         Task<(bool GetGuid, int RetVal, string Msg)> GetGUIDBasedOnUserAccountRoleGuid(string UserGuid);
         Task<(bool GetGuid, int RetVal, string Msg)> GetGUIDBasedOnRoleGuid(string UserGuid);
+        Task<(bool GetGuid, int RetVal, string Msg)> GetGUIDBasedOnOrgName(string UserGuid);
+        Task<(bool GetGuid, int RetVal, string Msg)> GetGUIDBasedOnUserPolicy(string UserGuid);
 
     }
 }
