7aedfe9658a4079857d67eb234bf9b02085e573b
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-02-24 07:24:41 +0530

    Change the GUID

--- DataAccessLayer/Interface/IUserGroupDAL.cs
+++ DataAccessLayer/Interface/IUserGroupDAL.cs
@@ -5,11 +5,12 @@ namespace DataAccessLayer.Interface
 {
     public interface IUserGroupDAL
     {
-        Task<List<UserGroupModel?>> GetAllUserPolicy();
-        Task<bool> InsertUpdateUserPolicy(UserGroupModel? UM);
-        Task<bool> DeleteUserPolicy(int Id);
-        Task<UserGroupModel?> GetUserPolicyById(int? Id);
-        Task<bool> UpdateUserPolicyAsync(int? id, UserGroupModel? userAccount);
+        Task<List<GetUserGroupModel?>> GetAllUserPolicy();
+        Task<(bool InsertUserGroup, int RetVal, string Msg)> InsertUpdateUserPolicy(UserGroupModel? UM);
+
+        Task<(bool deleteuserGroup, List<DeleteUserGroupResult> deleteResults)> DeleteUserPolicy(long UpdatedBy,DeleteUserGroup deleteUserGroup);
+        Task<GetUserGroupModel?> GetUserPolicyByGUId(string? GUId);
+        Task<(bool UpdateUserGroup, int RetVal, string Msg)> UpdateUserPolicyAsync(UpdateUserGroupModel? userAccount);
     }
 
 }
