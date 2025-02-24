70c2ca73354b4d1d92ac3818b1a75d1480fa46e9
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-02-24 07:19:56 +0530

    added the Org Name and User Policy

--- Services/GUID.cs
+++ Services/GUID.cs
@@ -1,6 +1,6 @@
 ﻿using DataAccessLayer.Uow.Implementation;
 using DataAccessLayer.Uow.Interface;
-using System.Reflection.Metadata.Ecma335;
+
 using WebApi.Controllers;
 
 namespace WebApi.Services
@@ -18,7 +18,7 @@ namespace WebApi.Services
             _httpContextAccessor = httpContextAccessor;
         }
 
-        public async Task<string> GetGUIDBasedOnUserGuid(string UpdatedGuidBy)
+        public async Task<string> GetGUIDBasedOnUserGuid(string? UpdatedGuidBy)
         {
             string response = string.Empty;
             try
@@ -100,6 +100,80 @@ namespace WebApi.Services
                 using (IUowGUID _repo = new UowGUID(_httpContextAccessor))
                 {
                     var result = await _repo.GUIDDALRepo.GetGUIDBasedOnRoleGuid(UpdatedGuidBy);
+
+                    _repo.Commit();
+                    if (result.GetGuid == true || result.GetGuid == false)
+                    {
+                        switch (result.RetVal)
+                        {
+                            case 1:// Success
+                                response = result.Msg;
+                                break;
+                            case 0:// Failure
+                                response = result.Msg;
+                                break;
+
+                            default:
+                                response = "Invalid User";
+                                break;
+
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error processing GUID: {ex.Message} - {ex.StackTrace}");
+            }
+            return response;
+        }
+
+        public async Task<string> GetGUIDBasedOnOrgName(string? UpdatedGuidBy)
+        {
+            string response = string.Empty;
+            try
+            {
+
+                using (IUowGUID _repo = new UowGUID(_httpContextAccessor))
+                {
+                    var result = await _repo.GUIDDALRepo.GetGUIDBasedOnOrgName(UpdatedGuidBy);
+
+                    _repo.Commit();
+                    if (result.GetGuid == true || result.GetGuid == false)
+                    {
+                        switch (result.RetVal)
+                        {
+                            case 1:// Success
+                                response = result.Msg;
+                                break;
+                            case 0:// Failure
+                                response = result.Msg;
+                                break;
+
+                            default:
+                                response = "Invalid User";
+                                break;
+
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error processing GUID: {ex.Message} - {ex.StackTrace}");
+            }
+            return response;
+        }
+
+        public async Task<string> GetGUIDBasedOnUserPolicy(string? UpdatedGuidBy)
+        {
+            string response = string.Empty;
+            try
+            {
+
+                using (IUowGUID _repo = new UowGUID(_httpContextAccessor))
+                {
+                    var result = await _repo.GUIDDALRepo.GetGUIDBasedOnUserPolicy(UpdatedGuidBy);
 
                     _repo.Commit();
                     if (result.GetGuid == true || result.GetGuid == false)
