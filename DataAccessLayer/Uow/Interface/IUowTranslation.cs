14008408a2f8b0db0faea08656be7e05ad3c870c
AndrewsChrispin <andrews.john@intercorpsolutions.in>
2025-03-01 00:21:42 +0530

    Multiple language Transulation

---
+++ DataAccessLayer/Uow/Interface/IUowTranslation.cs
@@ -0,0 +1,21 @@
+﻿using DataAccessLayer.Interface;
+using DataAccessLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Uow.Interface
+{
+    public interface IUowTranslation
+    {
+        ITranslationDAL TranslationDALRepo { get; }
+        void Commit();
+
+        Task<List<TranslationModel>> GetAllTranslations();
+
+        // Add the missing method
+        //Task<string?> GetTranslation(string key, string languageCode, string scope);
+    }
+}
