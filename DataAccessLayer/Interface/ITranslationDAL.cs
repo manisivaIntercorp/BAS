e6fc712f206237a3e09a02e72ea23f89c4259f4f
AndrewsChrispin <andrews.john@intercorpsolutions.in>
2025-03-01 00:19:17 +0530

    Multiple language Transulation

---
+++ DataAccessLayer/Interface/ITranslationDAL.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Interface
+{
+    public interface ITranslationDAL
+    {
+      //Task<string?> GetTranslation(string key, string languageCode);
+      //  Task<bool> InsertOrUpdateTranslation(TranslationModel translation);
+        Task<bool> InsertOrUpdateTranslationList(List<Translation> translation);
+                   
+        Task<List<TranslationModel>> GetAllTranslations();
+        Task<List<TranslationModel>> ExportResourceFiles(string ResourceName, string? Culture);
+
+        //Task<string?> GetTranslation(string key, string languageCode, string scope); // Updated method signature
+
+    }
+}
