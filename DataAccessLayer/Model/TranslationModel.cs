6b62a5ad0895151cfb40a44564d6864421a942dc
AndrewsChrispin <andrews.john@intercorpsolutions.in>
2025-03-01 00:14:21 +0530

    Multiple language Transulation

---
+++ DataAccessLayer/Model/TranslationModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Model
+{
+    public class TranslationModel
+    {
+        public int? Id { get; set; }
+        public string? Key { get; set; }
+        public string? LanguageCode { get; set; }
+        public string? Value { get; set; }
+        public string? Scope { get; set; }
+        public string? NeutralValue { get; set; }
+    }
+
+    public class Translation
+    {
+        public string? ResourceName { get; set; }
+        public string? TranslationKey { get; set; }
+        public string? LanguageCode { get; set; }
+        public string? TranslationValue { get; set; }
+    }
+}
