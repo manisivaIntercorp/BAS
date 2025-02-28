7da384ca23100ce60ca2251c0ed377d53719a7ce
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-02-28 09:45:09 +0530

    GUID DAL

--- DataAccessLayer/Implementation/GUIDDAL.cs
+++ DataAccessLayer/Implementation/GUIDDAL.cs
@@ -40,7 +40,7 @@ namespace DataAccessLayer.Implementation
             return (res, RetVal, Msg);
         }
 
-        public async Task<(bool GetGuid, int RetVal, string Msg)> GetGUIDBasedOnUserAccountRoleGuid(string UserGuid)
+        public async Task<(bool GetGuid, int RetVal, string Msg)> GetGUIDBasedOnUserAccountRoleGuid(string? UserGuid)
         {
             DynamicParameters parameters = new DynamicParameters();
             parameters.Add("@Mode", "GET_USERAccountRoleGUID");
@@ -63,7 +63,7 @@ namespace DataAccessLayer.Implementation
             return (res, RetVal, Msg);
         }
 
-        public async Task<(bool GetGuid, int RetVal, string Msg)> GetGUIDBasedOnRoleGuid(string UserGuid)
+        public async Task<(bool GetGuid, int RetVal, string Msg)> GetGUIDBasedOnRoleGuid(string? UserGuid)
         {
             DynamicParameters parameters = new DynamicParameters();
             parameters.Add("@Mode", "GET_ROLEGUID");
@@ -86,7 +86,7 @@ namespace DataAccessLayer.Implementation
             return (res, RetVal, Msg);
         }
 
-        public async Task<(bool GetGuid, int RetVal, string Msg)> GetGUIDBasedOnOrgName(string UserGuid)
+        public async Task<(bool GetGuid, int RetVal, string Msg)> GetGUIDBasedOnOrgName(string? UserGuid)
         {
             DynamicParameters parameters = new DynamicParameters();
             parameters.Add("@Mode", "GET_ORGGUID");
@@ -109,11 +109,57 @@ namespace DataAccessLayer.Implementation
             return (res, RetVal, Msg);
         }
 
-        public async Task<(bool GetGuid, int RetVal, string Msg)> GetGUIDBasedOnUserPolicy(string UserGuid)
+        public async Task<(bool GetGuid, int RetVal, string Msg)> GetGUIDBasedOnUserPolicy(string? UserGuid)
         {
             DynamicParameters parameters = new DynamicParameters();
             parameters.Add("@Mode", "GET_UserPolicyGUID");
             parameters.Add("@UpdatedBy", UserGuid);
+            parameters.Add("@RetVal", dbType: DbType.Int32, direction: ParameterDirection.Output);
+            parameters.Add("@Msg", dbType: DbType.String, size: 200, direction: ParameterDirection.Output);
+            var multi = await Connection.QueryMultipleAsync("sp_GetGUID",
+                parameters,
+                transaction: Transaction,
+                commandType: CommandType.StoredProcedure);
+            var guid = (await multi.ReadAsync<GUIDModel>()).ToList();
+            while (!multi.IsConsumed)
+            {
+                await multi.ReadAsync();
+            }
+            bool res = guid.Any();
+            int RetVal = parameters.Get<int?>("@RetVal") ?? -4;
+            string Msg = parameters.Get<string?>("@Msg") ?? "No Records Found";
+
+            return (res, RetVal, Msg);
+        }
+
+        public async Task<(bool GetGuid, int RetVal, string Msg)> GetGUIDBasedOnNationality(string? UserGuid)
+        {
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("@Mode", "GET_NationalityGUID");
+            parameters.Add("@UpdatedBy", UserGuid);
+            parameters.Add("@RetVal", dbType: DbType.Int32, direction: ParameterDirection.Output);
+            parameters.Add("@Msg", dbType: DbType.String, size: 200, direction: ParameterDirection.Output);
+            var multi = await Connection.QueryMultipleAsync("sp_GetGUID",
+                parameters,
+                transaction: Transaction,
+                commandType: CommandType.StoredProcedure);
+            var guid = (await multi.ReadAsync<GUIDModel>()).ToList();
+            while (!multi.IsConsumed)
+            {
+                await multi.ReadAsync();
+            }
+            bool res = guid.Any();
+            int RetVal = parameters.Get<int?>("@RetVal") ?? -4;
+            string Msg = parameters.Get<string?>("@Msg") ?? "No Records Found";
+
+            return (res, RetVal, Msg);
+        }
+
+        public async Task<(bool GetGuid, int RetVal, string Msg)> GetGUIDBasedOnMailServer(string? updatedGuidBy)
+        {
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("@Mode", "GET_MailServerGUID");
+            parameters.Add("@UpdatedBy", updatedGuidBy);
             parameters.Add("@RetVal", dbType: DbType.Int32, direction: ParameterDirection.Output);
             parameters.Add("@Msg", dbType: DbType.String, size: 200, direction: ParameterDirection.Output);
             var multi = await Connection.QueryMultipleAsync("sp_GetGUID",
