c8633b22f1aeea01249f96fcec7228817b3acc73
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-03-12 21:45:30 +0530

    Added User ID in Selecting GUID

--- DataAccessLayer/Interface/IUserGroupDAL.cs
+++ DataAccessLayer/Interface/IUserGroupDAL.cs
@@ -5,7 +5,7 @@ namespace DataAccessLayer.Interface
 {
     public interface IUserGroupDAL
     {
-        Task<List<GetUserGroupModel?>> GetAllUserPolicy();
+        Task<List<GetUserGroupModel?>> GetAllUserPolicy(long UpdatedBy);
         Task<(bool InsertUserGroup, int RetVal, string Msg)> InsertUpdateUserPolicy(UserGroupModel? UM);
 
         Task<(bool deleteuserGroup, List<DeleteUserGroupResult> deleteResults)> DeleteUserPolicy(long UpdatedBy,DeleteUserGroup deleteUserGroup);
