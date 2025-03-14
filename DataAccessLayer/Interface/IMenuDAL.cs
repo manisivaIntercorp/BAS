4cc5978247719f95df9c32d89833acfadbc4e96e
AndrewsChrispin <andrews.john@intercorpsolutions.in>
2025-03-14 19:12:53 +0530

    menu

---
+++ DataAccessLayer/Interface/IMenuDAL.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Interface
+{
+    public interface IMenuDAL
+    {
+        Task<List<MenuModel>> GetAllMenu();
+        Task<List<MenusModel>> GetMenu(string UserName, string ClientCode, int OrgID);
+    }
+}
