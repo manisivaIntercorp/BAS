39a233b7e68063203bdbc1c3a1be95668294690d
kaarthick-bhaskaran <kaarthick.bhaskaran@intercorpsolutions.in>
2025-03-19 21:57:29 +0530

    Added Modules for Insert

--- DataAccessLayer/Interface/IRoleDAL.cs
+++ DataAccessLayer/Interface/IRoleDAL.cs
@@ -10,6 +10,7 @@ namespace DataAccessLayer.Interface
         Task<(List<GetRoleModel?> roleModels, long? RetVal, string? Msg)> UpdateRole(GetRoleModel? LM);
         Task<(bool? DeleteRole, List<DeleteRoleInformation?> deleteRoleInformation)> DeleteRole(RolesDelete? rolesDelete,long UserId );
         Task<(RoleModel? rolemodel,List<Modules?> ModuleDatatable)> getModulesBasedOnRole(string? RoleGUID, long? UserGUID );
+        Task<List<Modules?>> getModulesBasedOnInsertRole(long? UserGUID);
         Task<(List<GetRoleModel?> roleModels, long? RetVal, string? Msg)> EditUpdateRoleAsync(GetRoleModel roleModel);
     }
 }
