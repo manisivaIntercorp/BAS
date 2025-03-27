faf03e13166d743d49a5e4dea3ee2b1d9e097659
AndrewsChrispin <andrews.john@intercorpsolutions.in>
2025-03-28 00:06:55 +0530

    Client Org

---
+++ DataAccessLayer/Interface/IClientOrganisationDAL.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Interface
+{
+    public interface IClientOrganisationDAL
+    {
+        Task<List<OrganisationModel>> GetClientOrganisation();
+        Task<List<LevelInfoModel>> GetOrganisationLevelInfo(OrganisationLevelModel organisationLevelModel);
+        Task<string> OrganisationLevelEdit(OrganisationLevelModel organisationLevelModel);
+        Task<List<LevelInfoModel>> SelectOrganisationLevelInfo(OrganisationLevelModel organisationLevelModel);
+        Task<List<DeleteResultModel>> DeleteOrganisationLevel(List<LevelInfoDetails> levelInfoDetails, string struserGuid, string strMode);
+    }
+}
