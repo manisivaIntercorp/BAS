a7f06632e90f3a0194d06e8f2fda0e2c2d05c182
AndrewsChrispin <andrews.john@intercorpsolutions.in>
2025-03-28 00:07:52 +0530

    Dept

---
+++ DataAccessLayer/Interface/IDepartmentDAL.cs
@@ -0,0 +1,21 @@
+﻿using DataAccessLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Interface
+{
+    public interface IDepartmentDAL
+    {
+        Task<DepartmentModel> GetDepartment(DepartmentInput departmentInput);  // ✅ No 'public' needed
+        Task<DepartmentModel> ViewDepartment(DepartmentInput departmentInput);  // ✅ No 'public' needed
+
+        Task<string> InsertDepartmentDetails(DepartmentInput organisationLevelModel);
+        Task<string> UpdateDepartmentDetails(DepartmentInput organisationLevelModel);
+        Task<List<DeptDeleteResult>> DeleteDepartmentDetails(DepartmentInput departmentInput);
+        
+    }
+
+}
